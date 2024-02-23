254859a1e64471c6d888836cef559bed9ca552f2
Larry Wen <l.wen@griffith.edu.au>
2024-02-23 13:15:56 +1000

    initalize files

---
+++ data.js
@@ -0,0 +1,1 @@
+// This file hold data for this week's exercise, you should not modify this file.
---
+++ questions.js
@@ -0,0 +1,4 @@
+// this is the questions of this week's lab exercise.
+// You only need to edit this file.
+// Please check the instruction carefully.
+
